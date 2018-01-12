fef738fb75f4699d220fa1b3aace5a3c971b15a1
Camilo Vanegas <kmilo.vanegas@gmail.com>
2018-01-12 15:34:15 -0500

    ajustes 01/12/2018

--- wp-content/themes/dfsk_theme/includes/footer.php
+++ wp-content/themes/dfsk_theme/includes/footer.php
@@ -30,5 +30,14 @@
 </div>
 
 <?php wp_footer(); ?>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-112422655-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-112422655-1');
+</script>
 </body>
 </html>
