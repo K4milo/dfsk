4665c5c093d22c0b464df37dbcde54adb9f63041
k4milo <kmilo.vanegas@gmail.com>
2018-01-25 06:58:45 -0500

    ajustes 25/01/2018

--- wp-content/themes/dfsk_theme/includes/loops/content-simplevehicles.php
+++ wp-content/themes/dfsk_theme/includes/loops/content-simplevehicles.php
@@ -20,7 +20,13 @@ Vehiculo simple Posts Loop
             <article id="post_<?php the_ID()?>" class="single-rows--video-post col-md-4">
                 <figure class="the_thumb"><?php the_post_thumbnail('full'); ?></figure>
                 <h3><?php the_title(); ?></h3>
-                <div class="txt"><?php the_excerpt(); ?></div>
+                <div class="txt">
+                    <?php if(get_field('ficha_tecnica')):?>
+                        <a href="<?php the_field('ficha_tecnica'); ?>" target="_blank">
+                            <img src="<?php bloginfo('template_url');?>/img/misc/btn-ficha.png" alt="<?php the_title(); ?>">
+                        </a>
+                    <?php endif;?>   
+                </div>
             </article>
         <?php endwhile; ?>
         <?php else: get_template_part('includes/loops/content', 'none'); ?>
